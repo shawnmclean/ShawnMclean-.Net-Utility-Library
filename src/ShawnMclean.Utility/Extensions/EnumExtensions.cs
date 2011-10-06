cd20558ae27d4e426fad6067f93563cb41bd0b43
Shawn Mclean <mixmasterxp@gmail.com>
2011-10-05 22:33:06 -0500

    Added an Enum Extension to get list of enumerants for an enum

---
+++ src/ShawnMclean.Utility/Extensions/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShawnMclean.Utility.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get a list of enumerant of the enum
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enumObj"></param>
+        /// <returns></returns>
+        public static IList<TEnum> ToEnumList<TEnum>(this TEnum enumObj) where TEnum : struct
+        {
+            if (!typeof(TEnum).IsEnum) throw new ArgumentException("An Enumeration type is required.", "enumObj");
+
+            return (TEnum[])Enum.GetValues(typeof(TEnum));
+        }
+    }
+}
