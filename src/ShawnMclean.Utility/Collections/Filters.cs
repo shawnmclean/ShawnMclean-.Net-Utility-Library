46da6aaf2ad85c1ac61c5cbb456907ef2e4c7727
Shawn Mclean <mixmasterxp@gmail.com>
2011-10-06 01:38:42 -0500

    Added Pagination on IEnumerable.

---
+++ src/ShawnMclean.Utility/Collections/Filters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShawnMclean.Utility.Collections
+{
+    public static class Filters
+    {
+        public static IEnumerable<TSource> Page<TSource>(this IEnumerable<TSource> source, int pageNumber, int pageSize)
+        {
+            return source.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
