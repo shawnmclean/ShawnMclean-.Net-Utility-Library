21015313d847e4aec035f960e5ea20d9e0d29757
Shawn Mclean <mixmasterxp@gmail.com>
2011-10-06 03:06:34 -0500

    Added a test file for string converter.

---
+++ tests/ShawnMclean.Utility.Tests/Conversions/StringConverterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ShawnMclean.Utility.Tests.Conversions
+{
+    [TestFixture]
+    public class StringConverterTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(false, false);
+        }
+    }
+}
